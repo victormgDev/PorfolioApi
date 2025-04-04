55f4b6b6eab77edbdeda63b253d9ef08d6e9e24c
Victor Montes Dev <victormontesgarrido@gmail.com>
2025-04-04 20:32:58 +0200

    v2.3.1

--- Program.cs
+++ Program.cs
@@ -11,27 +11,21 @@ builder.Services.AddSwaggerGen();
 // Registrar el servicio de email
 builder.Services.AddScoped<IEmailService, EmailService>();
 
-// Configurar CORS
-// builder.Services.AddCors(options =>
-// {
-//     options.AddPolicy("AllowAngularApp", policy =>
-//     {
-//         var allowAnyOrigin = builder.Configuration.GetSection("Cors:AllowAnyOrigin").Get<string[]>();
-//         if (allowAnyOrigin != null && allowAnyOrigin.Length > 0)
-//         {
-//             policy.WithOrigins(allowAnyOrigin).AllowAnyMethod().AllowAnyHeader();
-//         }
-//         else
-//         {
-//             // En producción, incluir también la URL de tu aplicación Angular desplegada
-//             policy.WithOrigins(
-//                 "http://localhost:4200",
-//                 "https://www.victormontesgarrido.com" // Añade aquí la URL de tu frontend desplegado
-//             ).AllowAnyMethod().AllowAnyHeader().AllowCredentials();
-//         }
-//     });
-// });
-
+// Configurar CORS de forma explícita
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        // En producción, permitir el acceso solo desde los orígenes específicos
+        policy.WithOrigins(
+                "http://localhost:4200", 
+                "https://www.victormontesgarrido.com" // URL del frontend en producción
+            )
+            .AllowAnyMethod() // Permitir cualquier método HTTP
+            .AllowAnyHeader() // Permitir cualquier cabecera
+            .AllowCredentials(); // Permitir credenciales (si las necesitas)
+    });
+});
 
 var app = builder.Build();
 
@@ -41,30 +35,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-else
-{
-    // En entornos de producción, podemos seguir ofreciendo Swagger para debugging
-    // pero deberíamos considerar protegerlo o deshabilitarlo en producción final
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-// Considera omitir esta línea en producción si Railway solo maneja HTTP
-// O verifica si el entorno es de desarrollo para decidir
+// Usar HTTPS solo en desarrollo (en producción, Railway maneja HTTPS)
 if (app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
 
-// Usar Cors
-//app.UseCors("AllowAngularApp");
-app.UseCors(policy =>
-{
-    policy.WithOrigins("http://localhost:4200", "https://www.victormontesgarrido.com")
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
-});
+// Usar CORS
+app.UseCors("AllowAngularApp");
 
 app.UseAuthorization();
 app.MapControllers();
